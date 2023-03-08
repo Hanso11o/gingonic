558e12f991595acf793b22666b6ec412b6e168b1
Erofeev Denis <deniserofeev84@gmail.com>
2023-03-08 21:26:01 +0300

    modified main_test.go

--- main_test.go
+++ main_test.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
 func TestWork(t *testing.T) {
-	t.Errorf("very epic fail")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	request, _ := http.NewRequest("GET", "/albums", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, request)
+	if w.Code != http.StatusOK {
+		t.Fatal("status not ok")
 
+	}
+	t.Errorf("very epic fail")
 }
